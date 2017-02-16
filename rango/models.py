1fc696e742c2643a3651896fe2499819bb5caabe
Holly Pringle <1105443P@student.gla.ac.uk>
2017-02-16 16:35:27 +0000

    chapter 5

--- rango/models.py
+++ rango/models.py
@@ -10,12 +10,19 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    class Page(models.Model):
-        category = models.ForeignKey(Category)
-        title = models.CharField(max_length=128)
-        url = models.URLField()
-        views = models.IntegerField(default=0)
+    def __unicode__(self):
+        return self.name
 
-        def __str__(self):
-            return self.title
+class Page(models.Model):
+
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return self.title
 
